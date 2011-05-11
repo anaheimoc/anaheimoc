7db7cb5a09f210c98ca5ab0f1c07d1bd82030f10
ana10.dev4.webenabled.net <w_ana10@we11.webenabled.net>
2011-05-11 09:12:52 -0700

    deleted sites/all

--- sites/all/modules/custom/wysiwyg_mods/wysiwyg_styles.js
+++
@@ -1,8 +0,0 @@
-CKEDITOR.addStylesSet('my_styles',
-    [
-      	// Inline Styles
-    	{ name : 'CSS Style', element : 'span', attributes : { 'class' : 'my_style' } },
-    	{ name : 'Marker: Yellow', element : 'span', styles : { 'background-color' : 'Yellow' } },
-    	{ name : 'Marker: Green', element : 'span', styles : { 'background-color' : 'Lime' } },
-      { name : 'Pullquote', element : 'span', attributes : {'class' : 'pullquote'} }
-    ]);
