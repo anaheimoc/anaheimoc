ceae49dc66db2f66233968d142bfe91dd07ebaf9
Dann <dmanahan@anaheimoc.org>
2011-07-05 10:49:56 -0700

    made static heights for the coupon divs to compensate for little or large text. added spacing to the bloging copy. took out weather-bot for the time being.

--- sites/all/themes/anaheimoc/weather.tpl.php
+++ sites/all/themes/anaheimoc/weather.tpl.php
@@ -1,16 +1,15 @@
 <div class="weather">
   <!-- <p><strong><?php print $weather['real_name']; ?></strong></p> -->
-  	<div class="weather-temp">
-  		<!--  -->
-  		<a href="/node/22190"><?php print t("!temperature",
+  <div class="weather-temp">
+  	<a href="/node/22190"><?php print t("!temperature",
          	 array('!temperature' => $weather['temperature'])); ?></a>
   </div>
   <div class="weather-img">
   	<a href="/node/22190"><img src="<?php print $weather['image']['filename']; ?>" height="60" width="60" alt="<?php print $weather['condition']; ?>" title="<?php print $weather['condition']; ?>" /></a>
   </div>
-  <div class="weather-sponsor">
+  <!-- <div class="weather-sponsor">
   	<span><a href="/node/22190">Sponsored by: </a></span>
   		<a href="/node/22190"><img src="http://anaheimoc.dev/sites/all/modules/contrib/weather/images/day-overcast.png" title="Sponsored by:" /></a><br />
-  	</div>
+  	</div> -->
  
 </div>
